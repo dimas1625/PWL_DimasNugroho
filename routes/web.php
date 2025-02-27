339b384a2f88b2c55f0b94fab690f239727c8ba9
Dimas Nugroho <dimasceper358@gmail.com>
2025-02-27 21:11:12 +0700

    Jobsheet2_Routing_Controller_View

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/greeting', [WelcomeController::class,'greeting']);
+Route::get('/greeting', function () {
+        return view('blog.hello', ['name' => 'Dimas']);
+});
