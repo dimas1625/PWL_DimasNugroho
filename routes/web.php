e115f80e75b9e8bae3354e2401f9190862fd8905
Dimas Nugroho <dimasceper358@gmail.com>
2025-02-27 18:17:46 +0700

    Jobsheet2_Routing_Controller_View

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', [WelcomeController::class, 'hello']);
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
