2723b7952c992ee5ac31f9936687c4f0fab71145
Dimas Nugroho <dimasceper358@gmail.com>
2025-02-27 08:38:16 +0700

    Jobsheet2_Routing_Controller_View

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { 
-    return 'Selamat Datang';
+Route::get('/about', function () { 
+    return '2341720033, Muhammad Dimas Ajie Nugroho';
 });
