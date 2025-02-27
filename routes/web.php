371f78955b0a7c426828524964b57da2190848a0
Dimas Nugroho <dimasceper358@gmail.com>
2025-02-27 08:55:03 +0700

    Jobsheet2_Routing_Controller_View

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/articles/{id}', function ($Id) { 
-    return 'Halaman Artikel dengan ID '.$Id;
+Route::get('/user/{name?}', function ($name=null) { 
+    return 'Nama saya '.$name;
 });
